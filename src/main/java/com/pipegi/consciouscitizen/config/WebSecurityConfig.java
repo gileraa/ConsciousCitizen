d02b7e562f574ef325c5fbbe43a5df732727a00d
Mikhail Grushenkov <grushenkov.m@gmail.com>
2022-11-21 19:30:01 +0500

    fix back

--- src/main/java/com/pipegi/consciouscitizen/config/WebSecurityConfig.java
+++ src/main/java/com/pipegi/consciouscitizen/config/WebSecurityConfig.java
@@ -17,7 +17,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
-        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
+        http.cors().and().csrf().disable();
     }
 
     @Bean
