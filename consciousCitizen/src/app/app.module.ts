5de9b75307be1e5d6b4622496b742ae59829ef22
Mikhail Grushenkov <grushenkov.m@gmail.com>
2022-11-22 20:37:13 +0500

    fix back

--- consciousCitizen/src/app/app.module.ts
+++ consciousCitizen/src/app/app.module.ts
@@ -22,15 +22,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { NewMessageComponent } from './new-message/new-message.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { HttpClientModule } from '@angular/common/http';
-import { MatCardModule } from '@angular/material/card';
 import {
   MatFormFieldModule,
   MAT_FORM_FIELD_DEFAULT_OPTIONS,
 } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
 import { CreatedMessageComponent } from './created-message/created-message.component';
 
 const appRoutes: Routes = [
